0f4e070297d911ad33b6a7c750fc0fbc4a026a0c
alexkaasik <aleks.kaasik@outlook.com>
2024-03-26 01:11:07 +0200

    added function to change data

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from functions import *
 from sql import *
 
 li=[["id","autor_nimi","sunnikuupaev"],["id","zanr"],["id","pealkiri","valjaandmise_kuupaev","autor","zanr"]]
+la=[["autor_id","autor_nimi","sunnikuupaev"],["zanr_id","zanr_nimi"],["raamatud_id","pealkiri","valjaandmise_kuupaev","autor_id","zanr_id"]]
 
 gui = Tk()
 
@@ -56,7 +57,56 @@ def Print_List_Of_Data(table,mode):
             cells[(x,y)] = b
     if (mode == "dele"):
         Button(GuiScreen, text="go", command=lambda boola=boola, CBG=CBG, table=table,cells=cells:What_To_Delete(CBG,boola,table,cells)).grid(row=r+1,column=1)
+
+def CPrint_List_Of_Data(table,mode):
+    clearFrame()
+    cells = {}
+
+    match table.get():
+        case "Autorid":
+            s = SelectAutorid
+        case "Zanrid":
+            s = SelectZanrid
+        case "Raamatud":
+            s = SelectRaamatud
+
+    SqlQeury=Execute_Query_Read(conn,s)
+    ColumnTableHeader(table)
+    r = (len(SqlQeury))
+    w = (len(SqlQeury[0]))
+    for x in range(r):
+        for y in range(w):
+            b = Text( GuiScreen, width=13 ,height=2,bd=1,bg="#aaaaaa",relief="solid")
+            b.grid(row=x+1, column=y+1)
+            b.configure(state=NORMAL)
+            b.insert(END,f"{SqlQeury[x][y]}")
+            if (y == 0):
+               b.configure(state=DISABLED)
+            cells[(x,y)] = b
+
+    Button(GuiScreen, text="go", command=lambda table = table, cells=cells:PrintCA(table,cells)).grid(row=r+1,column=1)
+
+def PrintCA(table, cells):
+    match table.get():
+        case "Autorid":
+            s = SelectAutorid
+            i = 0
+        case "Zanrid":
+            s = SelectZanrid
+            i = 1
+        case "Raamatud":
+            s = SelectRaamatud
+            i = 2
+    SqlQuery=Execute_Query_Read(conn,s)
     
+    r = len(SqlQuery)
+    w = len(SqlQuery[0])
+    for x in range(r):
+        for y in range(w):
+            if ( str(SqlQuery[x][y]) != str(cells[(x,y)].get(1.0,END))[:-1] ):
+                Qe = f"UPDATE { table.get() } SET { la[i][y] } = '{ str(cells[(x,y)].get(1.0,END))[:-1] }' WHERE { table.get()[:-2].lower() }_id = { cells[(x,0)].get(1.0,END)[:-1] };"
+                Execute_Query(conn,Qe)
+
 def What_To_Delete(CBG,boola,table,cells):
     for ga in CBG:
         #print(cells[ga]['text'])
@@ -74,11 +124,13 @@ def What_table_to_use_RB(mode:str):
     R2 = Radiobutton(GuiScreen,text="Zanrid",variable=Rg,value="Zanrid")
     R3 = Radiobutton(GuiScreen,text="Raamatud",variable=Rg,value="Raamatud")
     if ( mode == "print" ):
-        B1 = Button(GuiScreen,text="Print",command=lambda Rg=Rg:Print_List_Of_Data(Rg,"print"))
+        B1 = Button(GuiScreen,text="print",command=lambda Rg=Rg:Print_List_Of_Data(Rg,"print"))
     elif (mode == "add"):
-        B1 = Button(GuiScreen,text="Print",command=lambda Rg=Rg:Adding_data_DB(Rg))
+        B1 = Button(GuiScreen,text="add",command=lambda Rg=Rg:Adding_data_DB(Rg,"add"))
+    elif (mode == "change"):
+        B1 = Button(GuiScreen,text="change",command=lambda Rg=Rg:CPrint_List_Of_Data(Rg,"change"))
     elif (mode == "dele"):
-        B1 = Button(GuiScreen,text="Print",command=lambda Rg=Rg:Print_List_Of_Data(Rg,"dele"))
+        B1 = Button(GuiScreen,text="dele",command=lambda Rg=Rg:Print_List_Of_Data(Rg,"dele"))
 
     R1.grid(row=0,column=0)
     R2.grid(row=0,column=1)
@@ -162,7 +214,7 @@ Menu = Button(gui,text = 'Print',bg="white",font="Arial 24",width=MaxW1, height=
 Menu.grid(row=0,column=0)
 Add = Button(gui,text = 'Add',bg="white",font="Arial 24",width=MaxW1,height=MaxH1,borderwidth=4, relief="solid",command=lambda:What_table_to_use_RB("add"))
 Add.grid(row=1,column=0)
-Change = Button(gui,text = 'Change',bg="white",font="Arial 24",width=MaxW1,height=MaxH1, borderwidth=4, relief="solid")
+Change = Button(gui,text = 'Change',bg="white",font="Arial 24",width=MaxW1,height=MaxH1, borderwidth=4, relief="solid",command=lambda:What_table_to_use_RB("change"))
 Change.grid(row=2,column=0)
 Delete = Button(gui,text = 'Delete',bg="white",font="Arial 24",width=MaxW1,height=MaxH1, borderwidth=4, relief="solid",command=lambda:What_table_to_use_RB("dele"))
 Delete.grid(row=3,column=0)
